33ba2b93cc9e05c3297d639458fca1a12a768c61
ezgii4 <149891441+ezgna@users.noreply.github.com>
2024-03-21 03:00:06 +0900

    scraping a few cleaned data

--- scraper.js
+++ scraper.js
@@ -1,46 +1,119 @@
 const puppeteer = require('puppeteer');
 
 async function scrapeLogin(username, password) {
-  const browser = await puppeteer.launch({headless: true});
+  const browser = await puppeteer.launch({headless: false});
+  console.log("Browser launched");
+
   const page = await browser.newPage();
   await page.goto('https://portal.nkz.ac.jp/portal/top.do', { waitUntil: 'networkidle0' });
+  console.log("Page loaded");
+
   await page.type('#userId', username);
   await page.type('#password', password);
   await page.click('#loginButton');
   await page.waitForNavigation({ waitUntil: 'networkidle0' });
-  const exampleContent = await page.evaluate(() => {
-    const text = document.querySelector('#link_0').textContent;
-    return text.replace(/\s+/g, ' ').trim();
-  });
-  await browser.close();
-  return exampleContent;
+  console.log("Logged in");
+
+  let allDetails = [];
+
+  for (let i = 0; i <= 9; i++) {
+    // 各リンクのIDを生成
+    const linkId = `#link_${i}`;
+    console.log(`Clicking ${linkId}`);
+
+    await page.click(linkId);
+    console.log(`Open the content page for ${linkId}`);
+
+    // 特定のセレクタが表示されるまで待機
+    await page.waitForSelector('#oaprfloatclose', { visible: true });
+    console.log(`Content page for ${linkId} fully loaded`);
+
+    // ここでページの詳細を収集
+    const rawDetails = await page.evaluate(() => {
+
+      let data = {
+        sender: "",
+        title: "",
+        content: "",
+        attachment: "",
+        datetime: ""
+      };
+      
+      // .labelクラスを持つ全ての要素を取得
+      const labels = document.querySelectorAll('.label');
+      
+      labels.forEach(label => {
+        // labelの次の兄弟要素（.line_y_label）を飛ばし、その次の兄弟要素（目的の.item）を取得
+        const nextItem = label.nextElementSibling ? label.nextElementSibling.nextElementSibling : null;
+        
+        if (nextItem && nextItem.classList.contains('item')) {
+          // ラベルのテキスト内容に応じてdataオブジェクトに情報を保存
+          switch (label.textContent.trim()) {
+            case "送信者":
+              data.sender = nextItem.innerHTML.trim();
+              break;
+            case "タイトル":
+              data.title = nextItem.innerHTML.trim();
+              break;
+            case "本文":
+              data.content = nextItem.innerHTML.trim();
+              break;
+            case "添付ファイル":
+              data.attachment = nextItem.innerHTML.trim();
+              break;
+            case "掲示期間":
+              data.datetime = nextItem.innerHTML.trim();
+              break;
+          }
+        }
+      });
+
+      return data;
+    });
+
+    const details = {
+      sender: rawDetails.sender,
+      title: cleanHtmlAndWhitespace(rawDetails.title),
+      content: cleanHtmlAndWhitespace(rawDetails.content),
+      attachment: cleanHtmlAndWhitespace(rawDetails.attachment),
+      datetime: cleanDateTime(rawDetails.datetime)
+    }
+
+    allDetails.push(details);
+    console.log("Details pushed to the array");
+    await page.evaluate(() => {
+      document.querySelector('#oaprfloatclose').click();
+    });
+    await page.waitForSelector('#logout', { visible: true });
+    console.log("Page loaded.");
+  }
+
+  console.log("All contents are scraped");
+  // await browser.close();
+  return allDetails;
+
+}
+
+function cleanHtmlAndWhitespace(text) {
+  // HTMLタグを取り除く
+  const noHtml = text.replace(/<[^>]*>/g, '');
+  // HTMLエンティティの&nbsp;を空白に置き換える
+  let cleanText = noHtml.replace(/&nbsp;/g, ' ');
+  // 連続する空白を一つの空白に置き換え、文字列の先頭と末尾の空白を取り除く
+  cleanText = cleanText.replace(/\s+/g, ' ').trim();
+  return cleanText;
+}
+
+function cleanDateTime(datetime) {
+  // \nで文字列を分割し、最初の部分を取得
+  const cleanedDatetime = datetime.split('\n')[0].trim();
+  return cleanedDatetime;
 }
 
 module.exports = scrapeLogin;
 
-// (async () => {
-//   const browser = await puppeteer.launch({headless: false}); // ブラウザのヘッドレスモードを無効にして動作を見る
-//   const page = await browser.newPage();
-//   await page.goto('https://portal.nkz.ac.jp/portal/top.do', { waitUntil: 'networkidle0' });
+// nis21074
+// #B19980810
 
-//   // ログインフォームにユーザー名とパスワードを入力
-//   await page.type('#userId', 'nis21074');
-//   await page.type('#password', '#B19980810');
 
-//   // ログインボタンをクリック
-//   await page.click('#loginButton');
 
-//   // ナビゲーションが完了し、ネットワーク活動が落ち着くのを待つ
-//   await page.waitForNavigation({ waitUntil: 'networkidle0' });
-
-//   // ログイン後のページから情報を取得
-//   const exampleContent = await page.evaluate(() => {
-//     const text = document.querySelector('#link_0').textContent;
-//     // すべての空白文字を一つのスペースに置換し、先頭と末尾の空白をtrimする
-//     return text.replace(/\s+/g, ' ').trim();
-//   });
-//   console.log(exampleContent);
-  
-
-//   // await browser.close();
-// })();
